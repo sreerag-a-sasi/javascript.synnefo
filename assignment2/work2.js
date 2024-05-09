98012e13f11645e0a3d0dc145079f833edd44461
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-09 14:42:28 +0530

    java script from synnefo solutions

--- assignment2/work2.js
+++ assignment2/work2.js
@@ -1,8 +1,8 @@
 let x = 10;
 const y = 20;
-let z = y-x;
+let z = y - x;
 x = y + z;
-console.log("x = ",x);
+console.log("x = ", x);
 
 let greeting = "Hello";
 if (greeting === "Hello") {
@@ -17,21 +17,19 @@ console.log(doubled);
 
 let colors = ["red", "green", "blue"];
 let favoriteColor = colors[2];
-console.log("favoriteColor : ",favoriteColor);
-
+console.log("favoriteColor : ", favoriteColor);
 
 function add(x, y) {
-  return (x + y);
+  return x + y;
 }
 
 let result = add(5);
 console.log(add(x, y));
 
-
 let person = {
   firstName: "John",
   lastName: "Doe",
-  age:22
+  age: 22,
 };
 
 console.log(person.age);
@@ -44,7 +42,7 @@ for (let i = 0; i <= numbers.length; i++) {
 function multiply(x, y) {
   return x * y;
 }
-console.log(multiply(x,y));
+console.log(multiply(x, y));
 
 let product = multiply(3, "2");
 console.log("Product:", product);
@@ -72,3 +70,28 @@ for (let student of students) {
 }
 
 console.log("End of the code");
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//string reverse
+//string palindrome
+//fibonacci series
+//multiplication table
+//time date print
+//digital clock
+//calculator
+//assignment link 1
+// array elements sum
+// average of an array element
+//minimum and  maximum value in an array
+//change the negative value in an array to positive
+//even numbers and odd numbers sum in an array
+//sum of squres elements of an array
+// index of the first even number using find methode in an array
+//find even and odd number in an array using filter function
+//make an array of double from an array using map function
+//find the product of all positive numbers in an array using reduse method and avoid negative numbers
+//
