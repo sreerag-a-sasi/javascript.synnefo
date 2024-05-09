b70c7fbea9958638e90b57f316d86a689d0369bf
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-09 16:00:13 +0530

    java script from synnefo solutions

--- js1/assignment2/work2.js
+++ js1/assignment2/work2.js
@@ -71,16 +71,71 @@ for (let student of students) {
 
 console.log("End of the code");
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//string reverse
+{
+  //Using Built-in Functions: You can use the split, reverse, and join methods to reverse a string
+  function reverseString(str) {
+    return str.split("").reverse().join("");
+  }
+
+  const originalString = "synnefo solutions";
+  const reversedString = reverseString(originalString);
+  console.log(`Original: ${originalString}`);
+  console.log(`Reversed: ${reversedString}`);
+}
+{
+  //Using a Loop: You can manually reverse the string by iterating through its characters:
+  function reverseStringLoop(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+    return reversed;
+  }
+
+  const originalString = "synnefo solutions";
+  const reversedString = reverseStringLoop(originalString);
+  console.log(`Original: ${originalString}`);
+  console.log(`Reversed: ${reversedString}`);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//string palindrome
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// const originalString = prompt("give me a word!");
+// const reversedString = reverseString(originalString);
+// if (reversedString === originalString) {
+//   console.log("the given word is a palindrome!");
+// } else {
+//   console.log("the given word is not a palindrome!");
+// }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//string reverse
-//string palindrome
 //fibonacci series
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //multiplication table
+// {
+//   var number = prompt("give me a number");
+//   console.log(`Multiplication table for ${number} is :`);
+//   for (let i = 1; i <= 10; i++) {
+//     const result = number * i;
+//     console.log(`${number} * ${i} = ${result}`);
+//   }
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //time date print
+
+
 //digital clock
 //calculator
 //assignment link 1
