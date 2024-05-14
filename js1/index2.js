eef2ee8b43ae104c69520d447821126218b68812
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-14 14:51:50 +0530

    java script from synnefo solutions

--- js1/index2.js
+++ js1/index2.js
@@ -1,49 +1,49 @@
 let obj = {
-    name :"Harry",
-    age:13,
-    mark : 100,
-    greeting : function (){
-        console.log(`Hai ${this.name}, your mark is ${this.mark}`);
-    }
-}
+  name: "Harry",
+  age: 13,
+  mark: 100,
+  greeting: function () {
+    console.log(`Hai ${this.name}, your mark is ${this.mark}`);
+  },
+};
 
-console.log("name : ",obj.name);
+console.log("name : ", obj.name);
 obj.greeting();
 
 //constructor function
-function Person(name,age,mark) {
-    this.name = name;
-    this.age = age;
-    this.mark = mark;
+function Person(name, age, mark) {
+  this.name = name;
+  this.age = age;
+  this.mark = mark;
 
-    this.greeting = function() {
-        console.log(`hello ${this.name}, your mark is ${this.mark}`);
-    }
+  this.greeting = function () {
+    console.log(`hello ${this.name}, your mark is ${this.mark}`);
+  };
 }
 
-const person1=new Person("john",14,99);
+const person1 = new Person("john", 14, 99);
 console.log("person1 : ", person1);
-console.log("name : ",person1.name);
+console.log("name : ", person1.name);
 person1.greeting();
 
-const person2=new Person("jane",15,98);
+const person2 = new Person("jane", 15, 98);
 console.log("person2 : ", person2);
-console.log("name : ",person2.name);
+console.log("name : ", person2.name);
 person2.greeting();
 
 person2.name = "loki";
-console.log("name : ",person2.name);
+console.log("name : ", person2.name);
 
 person2.printHai = function () {
-    console.log(`Hai ${this.name}!!!!!!`);
-}
+  console.log(`Hai ${this.name}!!!!!!`);
+};
 
 person2.printHai();
 
 // person1.printHai();  //gets error!!
 
 Person.prototype.printHai = function () {
-    console.log(`Hai ${this.name} !!!!`);
-}
+  console.log(`Hai ${this.name} !!!!`);
+};
 
 person1.printHai();
