e622ea93c7983edcf4a901bd6b2a2da691824582
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-16 14:49:45 +0530

    java script from synnefo solutions

--- js1/16-5-24.js
+++ js1/16-5-24.js
@@ -12,6 +12,6 @@ console.log("fourth line");
 // second line
 // fourth line
 // third line
-//because whenever javascript see's a asynchronous operation it will automatically stacked in a queue
+// because whenever javascript see's a asynchronous operation it will automatically stacked in a queue
 
 //Asynchronous opretaions can be time out functions event listner functions and etc..
