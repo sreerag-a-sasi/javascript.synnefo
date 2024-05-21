4eaa72abb8b3062206737b7a5f439cb9cdd6c0da
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-21 16:16:22 +0530

    java script from synnefo solutions

--- js1/ecommerse/21-5-24.js
+++ js1/ecommerse/21-5-24.js
@@ -1,22 +1,22 @@
 let xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://fakestoreapi.com/products?limit=20");
-xhr.onload = function() {
-console.log("xhr : ", xhr);
-let content = document.getElementById("container3");
-let response = xhr.response;
-console.log("response : ", response);
+xhr.onload = function () {
+  console.log("xhr : ", xhr);
+  let content = document.getElementById("container3");
+  let response = xhr.response;
+  console.log("response : ", response);
 
-console.log("type of (response) :", typeof response);
-const parsed_response = JSON.parse(response);
-console.log("parsed_response : ", parsed_response);
+  console.log("type of (response) :", typeof response);
+  const parsed_response = JSON.parse(response);
+  console.log("parsed_response : ", parsed_response);
 
-let card = "";
+  let card = "";
 
-for (let i = 0; i < parsed_response.length; i++) {
-  card =
-    card +
-    `<div class="card">
+  for (let i = 0; i < parsed_response.length; i++) {
+    card =
+      card +
+      `<div class="card">
             <img src="${parsed_response[i].image}" >
             <h4>${parsed_response[i].id}</h4>
             <h4>${parsed_response[i].title}</h4>
@@ -24,8 +24,8 @@ for (let i = 0; i < parsed_response.length; i++) {
             <h4>${parsed_response[i].rating.rate}</h4>
             <p>${parsed_response[i].description}</p>
             </div>`;
-}
+  }
 
-content.innerHTML = card;
-}
+  content.innerHTML = card;
+};
 xhr.send();
