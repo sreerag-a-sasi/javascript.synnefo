22ada18c6529b572baf2d8c75e6ec5125cd87659
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-23 16:08:05 +0530

    java script from synnefo solutions

--- js1/validation/script.js
+++ js1/validation/script.js
@@ -53,3 +53,29 @@ console.log("result9 : ",result9);//true
 const regexp10 = /good$/im; //m-flag repesents multiline matching
 const result10 = regexp10.test(str4);// checks if str4 ends with "good"
 console.log("result10 : ",result10);// true
+
+const regexp11 = /fishes?/im; //charactor before '?' is optional for matching
+const result11 = regexp11.test(str3);// it matches both fishe and fishes, 's' is optional
+console.log("result11 : ",result11);// true
+
+const regexp12 = /fish(es)?/im;// '()' represents groups
+const result12 = regexp12.test(str3);//it matches both fish and fishes, 'es' is optional
+console.log("result12 : ",result12);// true
+
+
+
+const regexp13 = /fishes*$/i;// the letter before '*' can occur multiple times in a string
+const result13 = regexp13.test(str3);//it matches fishe, fishes, fishesss...
+console.log("result13 : ",result13);//true
+
+const regexp14 = /fishes+$/i;//the letter before '+' also can occur multiple times in a string, but atleast one occurance is required
+const result14 = regexp14.test(str3);//here it matches fishes, fishesss..
+console.log("result14 : ",result14); // false
+
+const regexp15 = /fish.$/i;// any charecter can occur after fish, '.' represents any type of charecter
+const result15 = regexp15.test(str3);//here it matches fishe, fisha, fishb, fish1, fish2, ... , but does not matches fish, a charecter in place of '.' is a must
+console.log("result15 : ",result15);//true
+
+const regexp16 = /fish.*$/i;
+const result16 = regexp16.test(str3);// here it matches fishe, fishes, fishesss, ..., since due to '*' multiple charecters are matched
+console.log("result16 : ", result16);//true
