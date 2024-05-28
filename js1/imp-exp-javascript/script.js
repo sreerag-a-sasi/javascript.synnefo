af531392b3463921504d6a13bc601a1abb575a76
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-28 15:32:08 +0530

    java script from synnefo solutions

--- js1/imp-exp-javascript/script.js
+++ js1/imp-exp-javascript/script.js
@@ -9,3 +9,21 @@ printHai();
 printHai1();
 printHai2();
 printWelcome();
+
+
+
+
+
+
+// const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// function isValidEmail(email) {
+//     return emailPattern.test(email);
+// }
+
+// // Example usage:
+// const email1 = 'john.doe@example.com';
+// const email2 = 'invalid-email';
+
+// console.log(isValidEmail(email1)); // Output: true
+// console.log(isValidEmail(email2)); // Output: false
