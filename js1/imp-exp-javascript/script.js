8c47cd70c803ddc3708528b6d69c2b2d4dc1c33f
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-05-28 15:57:56 +0530

    java script from synnefo solutions

--- js1/imp-exp-javascript/script.js
+++ js1/imp-exp-javascript/script.js
@@ -15,15 +15,3 @@ printWelcome();
 
 
 
-// const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-// function isValidEmail(email) {
-//     return emailPattern.test(email);
-// }
-
-// // Example usage:
-// const email1 = 'john.doe@example.com';
-// const email2 = 'invalid-email';
-
-// console.log(isValidEmail(email1)); // Output: true
-// console.log(isValidEmail(email2)); // Output: false
---
+++ js1/user-form-validation/form.js
@@ -0,0 +1,16 @@
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function isValidEmail(email) {
+    return emailPattern.test(email);
+}
+const userDataForm = document.getElementById("userDataForm");
+
+userDataForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent form submission
+
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+// Example usage:
+const email = emailInput.value;
+console.log("email : ",isValidEmail(email));
+});
